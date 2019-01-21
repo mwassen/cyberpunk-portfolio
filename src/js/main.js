f1e2c3f3c063e6f9b3f367d54691dc5f45d1e8ab
Marcus W <marcuswassen@gmail.com>
2019-01-21 16:04:10 +0100

    basic three implement

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,34 @@
-let PIXI = require("pixi.js");
-let PIXIfilters = require("pixi-filters");
+const THREE = global.THREE = require('three');
+require('three/examples/js/loaders/GLTFLoader');
 
-let app = new PIXI.Application({
-	width: window.innerWidth,
-	height: window.innerHeight,
-	antialiasing: false,
-	transparent: true,
-	resolution: 1
+const scene = new THREE.Scene();
+const camera = new THREE.Camera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+	alpha: true
 });
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-let state;
-let counter = 0;
-let outlines = new PIXI.Container();
+const loader = new THREE.GLTFLoader();
+loader.load( 'src/assets/mswsn3d.glb', gltf => {
+	scene.add( gltf.scene );
+	gltf.setSize(30, 30);
+}, undefined, error => {
+	console.log(error);
+} );
 
-document.body.appendChild(app.view);
+camera.rotateX(90);
 
-PIXI.loader
-	.add("./src/img/logo.png")
-	.add("./src/img/logow.png")
-	.load(setup);
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
-function setup() {
-	let logo = new PIXI.Sprite(PIXI.loader.resources["./src/img/logo.png"].texture);
+// camera.position.z = 5;
 
-	logo.scale.set(0.3);
-	logo.anchor.set(0.5);
-	logo.position.set(window.innerWidth / 2, window.innerHeight / 2);
-
-	app.stage.addChild(outlines);
-	app.stage.addChild(logo);
-
-	
-
-	state = play;
-
-	app.ticker.add(delta => gameLoop(delta));
-};
-
-function gameLoop(delta) {
-	state(delta);
-};
-
-function play() {
-	if (counter % 20 === 0) {
-		let outlineinstance = new PIXI.Sprite(PIXI.loader.resources["./src/img/logo.png"].texture)
-		outlineinstance.scale.set(0.3);
-		outlineinstance.anchor.set(0.5);
-		outlineinstance.position.set(window.innerWidth / 2, window.innerHeight / 2);
-
-		outlines.addChild(outlineinstance);
-	}
-
-	outlines.children.forEach(element => {
-		element.scale.set(element.scale.y + 0.001);
-
-	});
-
-	if (outlines.children.length > 3) {
-		outlines.removeChildAt(0);
-		
-	}
-
-	counter++;
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
 }
+animate();
