f2db55d52d7d171f4ff1f698ac11d322f2d4e62e
Marcus W <marcuswassen@gmail.com>
2019-01-31 19:45:38 +0100

    cleared site & small changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,27 @@
-const THREE = global.THREE = require('three');
-require('three/examples/js/loaders/GLTFLoader');
+const THREE = global.THREE = require( 'three' );
+require( 'three/examples/js/loaders/GLTFLoader' );
 
 const scene = new THREE.Scene();
 const camera = new THREE.Camera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer({
+const renderer = new THREE.WebGLRenderer( {
 	alpha: true
-});
+} );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const loader = new THREE.GLTFLoader();
 loader.load( 'src/assets/mswsn3d.glb', gltf => {
+
 	scene.add( gltf.scene );
-	gltf.setSize(30, 30);
+
 }, undefined, error => {
+
 	console.log(error);
+
 } );
 
-camera.rotateX(90);
+camera.rotateX( 90 );
 
 // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 // var material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
@@ -28,7 +31,10 @@ camera.rotateX(90);
 // camera.position.z = 5;
 
 function animate() {
+
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+
 }
+
 animate();
