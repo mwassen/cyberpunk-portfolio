c02cf8932eeb7d388214142e443d471a0849d167
Marcus W <marcuswassen@gmail.com>
2019-03-23 22:44:22 +0100

    [PROD] Cleared shaders for mobile testing

--- src/shaders/fragment1.glsl
+++ src/shaders/fragment1.glsl
@@ -1,14 +1,14 @@
 precision highp float;
 
 uniform float time;
-varying vec2 vUv;
+// varying vec2 vUv;
 
 void main () {
 	// vec3 color = vec3(sin(time) + 1.0);
-	vec3 colorA = vec3(0.0, (sin(time / 10.0) + 1.5) * 0.4, 0.0);
-	vec3 colorB = vec3(0.0, 0.0, sin(time / 8.0) + 1.5);
+	// vec3 colorA = vec3(0.0, (sin(time / 10.0) + 1.5) * 0.4, 0.0);
+	// vec3 colorB = vec3(0.0, 0.0, sin(time / 8.0) + 1.5);
 
-	vec3 color = mix(colorA, colorB, sin(time * 0.2 + cos(vUv.x)));
+	// vec3 color = mix(colorA, colorB, sin(time * 0.2 + cos(vUv.x)));
 
-	gl_FragColor = vec4(color, 0.8 * (sin(vUv.y * 10.0) + 2.0 ));
+	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- src/shaders/vertex1.glsl
+++ src/shaders/vertex1.glsl
@@ -2,13 +2,12 @@
 precision highp float;
 
 uniform float time;
+// varying vec2 vUv;
 
-varying vec2 vUv;
 // varying vec3 vNormal;
 
-
 void main () {
-	vUv = uv / (sin( time) / 20.0);
+	// vUv = uv;
 
-	gl_Position = (projectionMatrix / (sin(time) + 1.5)) * modelViewMatrix * vec4(position * (sin(time / 100.0) + 1.5) / 2.0, 1.0 + sin((time / uv.y  * uv.x) / 20.0) / 20.0);
+	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
