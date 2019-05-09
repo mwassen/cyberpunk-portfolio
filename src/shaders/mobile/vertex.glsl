1188fcfd340a7c5d94cba7678d0f560406e612fb
Marcus W <marcuswassen@gmail.com>
2019-05-09 16:28:25 +0200

    Split shaders into folder and new mobile vertex

---
+++ src/shaders/mobile/vertex.glsl
@@ -0,0 +1,32 @@
+precision mediump float;
+
+attribute vec3 position;
+attribute vec2 uv;
+attribute vec3 normal;
+
+uniform float time;
+uniform float scroll;
+uniform float hoverVal;
+uniform float index;
+
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
+
+varying vec2 vUv;
+varying vec3 nposition;
+varying vec3 vNormal;
+
+void main(){
+	vUv=uv;
+	
+	vNormal=normal;
+	
+	vec3 offset=position;
+	float dist=(sin(time/10.)+1.)*.05*scroll+scroll;
+	
+	offset.xyz+=normal*dist;
+	
+	nposition=position;
+	
+	gl_Position=projectionMatrix*modelViewMatrix*vec4(offset,1.);
+}
